a93f2d24142d42a437e020f7ed77d4ee6f90b89f
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2021-08-04 05:33:19 -0700

    fill out .dir-locals.el a bit

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((rust-mode . ((rust-indent-offset . 2)))
- (rustic-mode . ((rustic-indent-offset . 2))))
+ (rustic-mode . ((rustic-indent-offset . 2)))
+ (prog-mode . ((fill-column . 100)
+               (highlight-80+-columns . 100))))
